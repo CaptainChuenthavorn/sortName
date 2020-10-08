3b401ce21d634ccf44fd9c14bb2bc33735901608
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-10-08 09:39:13 +0700

    Add project files.

---
+++ sortName/sortName.cpp
@@ -0,0 +1,36 @@
+﻿#define  _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+int SUM(int* pointerA, int n, int sum);
+int main()
+{
+    int n, i, * pointerA, sum = 0, choice;
+    printf("how many number you have? : ");
+    scanf("%d", &n);
+    //printf("What do you want to do : 1=sum 2=minus");
+    //scanf("%d", &choice);
+    //if(choice = 0)
+    pointerA = (int*)malloc(n * sizeof(int));
+
+    if (pointerA == NULL)
+    {
+        printf("Error! memory not allocated.");
+        exit(0);
+    }
+        
+    printf("Enter elements: ");
+   
+    printf("Sum = %d", SUM(pointerA, n, sum));
+
+  
+
+    return 0;
+}
+int SUM(int *pointerA,int n,int sum) {
+    for (int i = 0; i < n; ++i)
+    {
+        scanf("%d", pointerA + i);
+        sum += *(pointerA + i);
+    }
+    return sum;
+}
